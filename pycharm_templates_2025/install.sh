0825b0fea32019dd7a90aa96b7e38ab5b155c178
Roberto Saavedra <rsaavedrav@copelme.com>
2025-01-20 20:11:24 -0400

    add new templates

--- pycharm_templates_2025/install.sh
+++ pycharm_templates_2025/install.sh
@@ -5,7 +5,7 @@ TEMPLATE_FOLDER="${PYCHARM_TEMPLATES_FOLDER}"
 echo """$TEMPLATE_FOLDER"""
 # shellcheck disable=SC2054
 # eval """open $TEMPLATE_FOLDER"""
-EXPORT_FILES=('OdooBaseXml.xml' 'OdooXml.xml' 'OdooPython.xml')
+EXPORT_FILES=('AngularScript.xml' 'BashScript.xml' 'JavaScript.xml' 'OtherScript.xml' 'PythonScript.xml' 'ShellScript.xml' 'SQLScript.xml')
 for f_names in "${EXPORT_FILES[@]}" ; do
     echo "select file.... $f_names"
     eval """cp -p $f_names ${TEMPLATE_FOLDER}/$f_names"""
